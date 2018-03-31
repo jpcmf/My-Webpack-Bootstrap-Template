d31c117f1558c1dbb16de1678f15b0b1beff23b7
João Paulo <musicjoaopaulo@gmail.com>
2018-03-31 18:42:47 -0300

    Add files

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+import 'bootstrap/js/modal.js';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+// import 'jquery/dist/jquery.js';
+
+// import '../css/style.css';
+import '../sass/main.scss';
+// import '../styl/main.styl';
+
+$('h1').click(() => alert('teste'));
+var hello = 'Hello world!';
